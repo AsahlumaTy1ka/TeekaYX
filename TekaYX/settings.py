62c83c2ab38430ca6f8faba6c1532386eb2dff0d
AsahlumaTy1ka <asahlumamabhongo02@gmail.com>
2024-08-22 20:59:57 +0000

    Another one

--- TekaYX/settings.py
+++ TekaYX/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2#9defzm9quwg!jkb9id(+2=zl%&@r%5(vzuca!y@#gjqw_&v)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['TeekaYX.pythonanywhere.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['TeekaYX.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
--- main/urls.py
+++ main/urls.py
@@ -13,7 +13,11 @@ urlpatterns = [
     path('courses/<slug:slug>/', views.course_detail, name='course-detail'),
     path('courses/<slug:course_slug>/<slug:page_slug>/', views.course_page_detail, name='course-page-detail'),
     path('about/', views.about, name='about'),
+    path('privacy-policy/', views.policy, name='privacy-policy'),
+    path('terms/', views.terms, name='terms'),
+    path('contact-us/', views.contact, name='contact-us'),   
     path('', views.index, name='index'),
+    
 
 
 ]
